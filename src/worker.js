9fa812084a2802576caa085ad9dc9e844f1361d5
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-05-26 21:08:42 +0300

    upd

--- src/worker.js
+++ src/worker.js
@@ -1,11 +1,12 @@
 export const calculate = (value) => {
+	console.log('value', value);
 	const plainText = value.reduce(
 		(acc, { children }) =>
-			acc + children.reduce((acc, { text }) => acc + text, ''),
+			acc + ' ' + children.reduce((acc, { text }) => acc + text, ''),
 		''
 	);
 
-	const words = plainText.split(' ').filter((n) => n !== '').length;
+	const words = plainText.split(/\s+|\\n/g).filter((n) => n !== '').length;
 
 	const length = plainText.length;
 	return { characters: length, plainText, words };
